e5de9cb1ff95bcede2c6240665c1263276df18ce
Juan David Osorio <72204602+jd-oh@users.noreply.github.com>
2024-03-27 14:02:53 -0500

    Primer commit

---
+++ src/main/java/com/ucaldas/posgrados/PosgradosApplication.java
@@ -0,0 +1,13 @@
+package com.ucaldas.posgrados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosgradosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosgradosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucaldas/posgrados/PosgradosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucaldas.posgrados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosgradosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
