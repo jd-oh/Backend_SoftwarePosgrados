5223d57b69a96abcc3443b16c12185e62e9eb7e8
Juan David Osorio <72204602+jd-oh@users.noreply.github.com>
2024-05-13 10:54:12 -0500

    prueba login con parametros

--- src/main/java/com/ucaldas/posgrados/Auth/AutenticacionController.java
+++ src/main/java/com/ucaldas/posgrados/Auth/AutenticacionController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +18,8 @@ public class AutenticacionController {
     private final AuthService authService;
 
     @PostMapping(value = "/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<AuthResponse> login(@RequestParam String username, @RequestParam String password) {
+        LoginRequest loginRequest = new LoginRequest(username, password);
         return ResponseEntity.ok(authService.login(loginRequest));
     }
 
@@ -25,4 +27,5 @@ public class AutenticacionController {
     public ResponseEntity<AuthResponse> registro(@RequestBody RegisterRequest registerRequest) {
         return ResponseEntity.ok(authService.registro(registerRequest));
     }
+
 }
