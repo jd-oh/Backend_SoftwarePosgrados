7324e8217cb55a70f74f7c3653ad54aa58a7c6ce
Juan David Osorio <72204602+jd-oh@users.noreply.github.com>
2024-05-14 11:54:47 -0500

    Put Edicion Usuario

--- src/main/java/com/ucaldas/posgrados/Controller/UsuarioController.java
+++ src/main/java/com/ucaldas/posgrados/Controller/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,7 +48,7 @@ public class UsuarioController {
         return ResponseEntity.ok("Usuario activado");
     }
 
-    @PostMapping("editarDatosBasicos")
+    @PutMapping("editarDatosBasicos")
     public ResponseEntity<String> editarDatosBasicos(@RequestParam int id, @RequestParam String nombre,
             @RequestParam String apellido,
             @RequestParam String email) {
