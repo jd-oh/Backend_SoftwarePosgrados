ef7142762fdcf871e526ef1e44e72881f162898d
Juan David Osorio <72204602+jd-oh@users.noreply.github.com>
2024-05-26 12:46:52 -0500

    Corrección de error del @Data

--- src/main/java/com/ucaldas/posgrados/Entity/EjecucionPresupuestal.java
+++ src/main/java/com/ucaldas/posgrados/Entity/EjecucionPresupuestal.java
@@ -12,34 +12,47 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+
 public class EjecucionPresupuestal {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
     private int id;
 
     @ManyToOne
     @JoinColumn(name = "idPresupuesto", referencedColumnName = "id")
+    @Getter
+    @Setter
     private Presupuesto presupuesto;
 
     // Son todos los ingresos menos los descuentos
+    @Getter
+    @Setter
     private double ingresosTotales;
 
     // Incluye gastos personales(ServDocentes parcial, ServNoDocentes total,
     // OtrosServDocentes total)
     // gastos generales, otros gastos, transferencias y gastos de viaje
+    @Getter
+    @Setter
     private double egresosProgramaTotales;
 
     // Incluye gastos de inversiones, recurrentes adm y gastos personales
     // (ServDocentes parcial)
+    @Getter
+    @Setter
     private double egresosRecurrentesUniversidadTotales;
 
+    @Getter
+    @Setter
     private double balanceGeneral;
 
     @OneToMany(mappedBy = "ejecucionPresupuestal")
@@ -85,5 +98,95 @@ public class EjecucionPresupuestal {
     @OneToMany(mappedBy = "ejecucionPresupuestal")
     @JsonManagedReference
     private Set<EgresosTransferencias> egresosTransferenciasEjecucion;
+
+    // Getters y setters de los Set
+
+    public Set<EgresosDescuentos> getEgresosDescuentosEjecucion() {
+        return egresosDescuentosEjecucion;
+    }
+
+    public void setEgresosDescuentosEjecucion(Set<EgresosDescuentos> egresosDescuentosEjecucion) {
+        this.egresosDescuentosEjecucion = egresosDescuentosEjecucion;
+    }
+
+    public Set<Ingresos> getIngresosEjecucion() {
+        return ingresosEjecucion;
+    }
+
+    public void setIngresosEjecucion(Set<Ingresos> ingresosEjecucion) {
+        this.ingresosEjecucion = ingresosEjecucion;
+    }
+
+    public Set<EgresosInversiones> getEgresosInversionesEjecucion() {
+        return egresosInversionesEjecucion;
+    }
+
+    public void setEgresosInversionesEjecucion(Set<EgresosInversiones> egresosInversionesEjecucion) {
+        this.egresosInversionesEjecucion = egresosInversionesEjecucion;
+    }
+
+    public Set<EgresosOtros> getEgresosOtrosEjecucion() {
+        return egresosOtrosEjecucion;
+    }
+
+    public void setEgresosOtrosEjecucion(Set<EgresosOtros> egresosOtrosEjecucion) {
+        this.egresosOtrosEjecucion = egresosOtrosEjecucion;
+    }
+
+    public Set<EgresosOtrosServDocentes> getEgresosOtrosServDocentesEjecucion() {
+        return egresosOtrosServDocentesEjecucion;
+    }
+
+    public void setEgresosOtrosServDocentesEjecucion(Set<EgresosOtrosServDocentes> egresosOtrosServDocentesEjecucion) {
+        this.egresosOtrosServDocentesEjecucion = egresosOtrosServDocentesEjecucion;
+    }
+
+    public Set<EgresosServDocentes> getEgresosServDocentesEjecucion() {
+        return egresosServDocentesEjecucion;
+    }
+
+    public void setEgresosServDocentesEjecucion(Set<EgresosServDocentes> egresosServDocentesEjecucion) {
+        this.egresosServDocentesEjecucion = egresosServDocentesEjecucion;
+    }
+
+    public Set<EgresosServNoDocentes> getEgresosServNoDocentesEjecucion() {
+        return egresosServNoDocentesEjecucion;
+    }
+
+    public void setEgresosServNoDocentesEjecucion(Set<EgresosServNoDocentes> egresosServNoDocentesEjecucion) {
+        this.egresosServNoDocentesEjecucion = egresosServNoDocentesEjecucion;
+    }
+
+    public Set<EgresosGenerales> getEgresosGeneralesEjecucion() {
+        return egresosGeneralesEjecucion;
+    }
+
+    public void setEgresosGeneralesEjecucion(Set<EgresosGenerales> egresosGeneralesEjecucion) {
+        this.egresosGeneralesEjecucion = egresosGeneralesEjecucion;
+    }
+
+    public Set<EgresosRecurrentesAdm> getEgresosRecurrentesAdmEjecucion() {
+        return egresosRecurrentesAdmEjecucion;
+    }
+
+    public void setEgresosRecurrentesAdmEjecucion(Set<EgresosRecurrentesAdm> egresosRecurrentesAdmEjecucion) {
+        this.egresosRecurrentesAdmEjecucion = egresosRecurrentesAdmEjecucion;
+    }
+
+    public Set<EgresosViajes> getEgresosViajeEjecucion() {
+        return egresosViajeEjecucion;
+    }
+
+    public void setEgresosViajeEjecucion(Set<EgresosViajes> egresosViajeEjecucion) {
+        this.egresosViajeEjecucion = egresosViajeEjecucion;
+    }
+
+    public Set<EgresosTransferencias> getEgresosTransferenciasEjecucion() {
+        return egresosTransferenciasEjecucion;
+    }
+
+    public void setEgresosTransferenciasEjecucion(Set<EgresosTransferencias> egresosTransferenciasEjecucion) {
+        this.egresosTransferenciasEjecucion = egresosTransferenciasEjecucion;
+    }
 
 }
--- src/main/java/com/ucaldas/posgrados/Entity/Presupuesto.java
+++ src/main/java/com/ucaldas/posgrados/Entity/Presupuesto.java
@@ -13,46 +13,74 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+// No se usó la etiqueta @Data porque looombok no maneja bien los conjuntos Set.
+// Así que se usaron los @Getter y @Setter sólo para los atributos que no son
+// conjuntos Set.
 public class Presupuesto {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
     private int id;
 
     @OneToOne
     @JoinColumn(name = "idCohorte", referencedColumnName = "id")
+    @Getter
+    @Setter
     private Cohorte cohorte;
 
+    @Getter
+    @Setter
     private String estado;
 
+    @Getter
+    @Setter
     private String observaciones;
 
+    @Getter
+    @Setter
     // Son todos los ingresos menos los descuentos
     private double ingresosTotales;
 
+    @Getter
+    @Setter
     private String fechaHoraCreacion;
 
+    @Getter
+    @Setter
     private String fechaHoraUltimaModificacion;
 
+    @Getter
+    @Setter
     private String fechaHoraEnviadoRevision;
 
+    @Getter
+    @Setter
     private String fechaHoraAprobado;
 
     // Incluye gastos personales(ServDocentes parcial, ServNoDocentes total,
     // OtrosServDocentes total)
     // gastos generales, otros gastos, transferencias y gastos de viaje
+    @Getter
+    @Setter
     private double egresosProgramaTotales;
 
     // Incluye gastos de inversiones, recurrentes adm y gastos personales
     // (ServDocentes parcial)
+    @Getter
+    @Setter
     private double egresosRecurrentesUniversidadTotales;
 
+    @Getter
+    @Setter
     private double balanceGeneral;
 
     @OneToMany(mappedBy = "presupuesto", cascade = CascadeType.ALL, orphanRemoval = true)
@@ -98,5 +126,95 @@ public class Presupuesto {
     @OneToMany(mappedBy = "presupuesto", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private Set<EgresosTransferencias> egresosTransferencias;
+
+    // Getter y Setter de los Set
+
+    public Set<EgresosDescuentos> getEgresosDescuentos() {
+        return egresosDescuentos;
+    }
+
+    public void setEgresosDescuentos(Set<EgresosDescuentos> egresosDescuentos) {
+        this.egresosDescuentos = egresosDescuentos;
+    }
+
+    public Set<Ingresos> getIngresos() {
+        return ingresos;
+    }
+
+    public void setIngresos(Set<Ingresos> ingresos) {
+        this.ingresos = ingresos;
+    }
+
+    public Set<EgresosInversiones> getEgresosInversiones() {
+        return egresosInversiones;
+    }
+
+    public void setEgresosInversiones(Set<EgresosInversiones> egresosInversiones) {
+        this.egresosInversiones = egresosInversiones;
+    }
+
+    public Set<EgresosOtros> getEgresosOtros() {
+        return egresosOtros;
+    }
+
+    public void setEgresosOtros(Set<EgresosOtros> egresosOtros) {
+        this.egresosOtros = egresosOtros;
+    }
+
+    public Set<EgresosOtrosServDocentes> getEgresosOtrosServDocentes() {
+        return egresosOtrosServDocentes;
+    }
+
+    public void setEgresosOtrosServDocentes(Set<EgresosOtrosServDocentes> egresosOtrosServDocentes) {
+        this.egresosOtrosServDocentes = egresosOtrosServDocentes;
+    }
+
+    public Set<EgresosServDocentes> getEgresosServDocentes() {
+        return egresosServDocentes;
+    }
+
+    public void setEgresosServDocentes(Set<EgresosServDocentes> egresosServDocentes) {
+        this.egresosServDocentes = egresosServDocentes;
+    }
+
+    public Set<EgresosServNoDocentes> getEgresosServNoDocentes() {
+        return egresosServNoDocentes;
+    }
+
+    public void setEgresosServNoDocentes(Set<EgresosServNoDocentes> egresosServNoDocentes) {
+        this.egresosServNoDocentes = egresosServNoDocentes;
+    }
+
+    public Set<EgresosGenerales> getEgresosGenerales() {
+        return egresosGenerales;
+    }
+
+    public void setEgresosGenerales(Set<EgresosGenerales> egresosGenerales) {
+        this.egresosGenerales = egresosGenerales;
+    }
+
+    public Set<EgresosRecurrentesAdm> getEgresosRecurrentesAdm() {
+        return egresosRecurrentesAdm;
+    }
+
+    public void setEgresosRecurrentesAdm(Set<EgresosRecurrentesAdm> egresosRecurrentesAdm) {
+        this.egresosRecurrentesAdm = egresosRecurrentesAdm;
+    }
+
+    public Set<EgresosViajes> getEgresosViaje() {
+        return egresosViaje;
+    }
+
+    public void setEgresosViaje(Set<EgresosViajes> egresosViaje) {
+        this.egresosViaje = egresosViaje;
+    }
+
+    public Set<EgresosTransferencias> getEgresosTransferencias() {
+        return egresosTransferencias;
+    }
+
+    public void setEgresosTransferencias(Set<EgresosTransferencias> egresosTransferencias) {
+        this.egresosTransferencias = egresosTransferencias;
+    }
 
 }
